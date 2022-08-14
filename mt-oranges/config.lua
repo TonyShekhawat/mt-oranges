037891c10f284e8324957fb21dfb5df6d945d5fe
Marttins <89866234+Marttins011@users.noreply.github.com>
2022-08-14 13:15:20 +0100

    Update config.lua

--- mt-oranges/config.lua
+++ mt-oranges/config.lua
@@ -4,7 +4,6 @@ Config.CarName = 'kalahari'
 Config.CarSpawnLoc = vector4(423.92, 6514.85, 27.67, 171.03)
 Config.CarSpawnPed = {416.34, 6520.76, 26.72,"Farm Guy Borro",266.3,0x94562DD7,"a_m_m_farmer_01"}
 Config.CarPedLoc = vector3(416.34, 6520.76, 26.72)
-Config.CarMapBlipLoc = AddBlipForCoord(416.34, 6520.76, 26.72)
 
 Config.OrangesField = vector3(354.88, 6516.23, 28.25)
 Config.OrangesProcess = vector3(1961.89, 5179.48, 47.94)
